1a752eb90e83e8e56af14b362575ce2ed239bc3e
Pedro <ThanksUniverse333@outlook.com>
2023-06-20 09:02:17 -0300

    Dictionary Learn

--- Program.cs
+++ Program.cs
@@ -4,60 +4,18 @@
     {
         public static void Main()
         {
-            Player pedro = new("Pedro", 20, "Brazil", 100.37);
-            Player pedro2 = new("Pedro", 20, "Brazil", 100.37);
-            Player pedro3 = new("Pedro", 20, "Brazil", 100.37);
-            var friends = new List<string> { "Pedro", "Pedro2", "Myko", "Pedro3", "Tay", "Pedro4", "Joe", "Pedro5", "Andy", "Bi", "BiggestName" };
-            Console.WriteLine("Say how many people you want in the party.");
-            string? inviteFriends = Console.ReadLine();
-            int invited;
-            while (!int.TryParse(inviteFriends, out invited) || (invited >= friends.Count || invited <= 0))
+            var myDictionary = new Dictionary<int, string>();
+            //List<string> myList = new();
+            myDictionary.Add(1, "Something");
+            myDictionary.Add(2, "Something");
+            myDictionary.Add(3, "Something");
+
+            foreach (var item in myDictionary)
             {
-                Console.WriteLine("Say how many people you want in the party.");
-                inviteFriends = Console.ReadLine();
+                Console.WriteLine(item);
             }
-            var partyFriends = GetPartyFriends(friends, invited);
 
-            string friendList = "";
-
-            foreach (var name in partyFriends)
-                friendList = friendList + name + ", ";
-
-
-            friendList = friendList.Remove(friendList.Length - 2);
-            Console.WriteLine(friendList);
-        }
-
-        public static List<string> GetPartyFriends(List<string> list, int count)
-        {
-            if (list == null)
-                throw new ArgumentNullException("List", "The database seems to be offline or you doesnt have internet connection, try again later.");
-
-            if (count > list.Count || count <= 0)
-                throw new ArgumentOutOfRangeException("count", "Count cannot be greater than partyFriends size.");
-
-            var partyFriends = new List<string>();
-
-            while (partyFriends.Count < count)
-            {
-                var currentFriend = GetPartyFriend(list);
-                partyFriends.Add(currentFriend);
-                list.Remove(currentFriend);
-            }
-            return partyFriends;
-        }
-
-        public static string GetPartyFriend(List<string> list)
-        {
-            string shortestName = list[0];
-            for (var i = 0; i < list.Count; i++)
-            {
-                if (list[i].Length < shortestName.Length)
-                {
-                    shortestName = list[i];
-                }
-            }
-            return shortestName;
+            Console.WriteLine(myDictionary[2]);
         }
     }
 }
